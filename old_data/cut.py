0b9db21cf66ceeba7288348b9f766d46d09ba991
anomius <swarajpurohit@gmail.com>
2020-08-01 17:15:40 +0530

    tiff

---
+++ old_data/cut.py
@@ -0,0 +1,17 @@
+import numpy as np
+from PIL import Image
+import timeit
+start = timeit.default_timer() 
+
+dt =np.dtype(np.int16)
+l= np.fromfile('black.rl0', dtype=dt).reshape((8238,6000))
+for i in range(20):
+    for j in range(25):
+        x=320*j
+        y=300*i
+        k=l[x:x+320,y:y+300]
+        s=str(i)+" "+str(j)
+        s="../"+s+'.tif'
+        Image.fromarray(k).save(s)
+stop = timeit.default_timer()
+print('Time: ', stop - start)
