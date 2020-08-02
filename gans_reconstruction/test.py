6daf44c568a6e05b28f3cc7ff2161a14bc22080c
Vignesh Charan <vigneshcharan26@gmail.com>
2020-08-02 23:18:37 +0530

    test,py

--- gans_reconstruction/test.py
+++ gans_reconstruction/test.py
@@ -92,6 +92,7 @@ if __name__ == "__main__":
 
     h, w, _ = image.shape
     grid = 8
+    image1 = image1[:h//grid*grid, :w//grid*grid] 
     image = image[:h//grid*grid, :w//grid*grid,:]
     mask = mask[:h//grid*grid, :w//grid*grid,:]
     print('Shape of image: {}'.format(image.shape))
