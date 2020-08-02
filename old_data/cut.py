b2868eaab47d8ee18bf91c2acc0524cdf541e1d2
anomius <swarajpurohit@gmail.com>
2020-08-02 11:40:54 +0530

    cut

--- old_data/cut.py
+++ old_data/cut.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 import timeit
+from tifffile import imsave
 start = timeit.default_timer() 
 
 dt =np.dtype(np.int16)
@@ -10,8 +11,8 @@ for i in range(18):
         x=320*j
         y=320*i
         k=l[x:x+320,y:y+320]
-        s=str(i)+" "+str(j)
-        s="../"+s+'.tif'
-        Image.fromarray(k).save(s)
+        s=str(i)+"_"+str(j)
+        s=s+'.tif'
+        imsave(s,k)
 stop = timeit.default_timer()
 print('Time: ', stop - start)
