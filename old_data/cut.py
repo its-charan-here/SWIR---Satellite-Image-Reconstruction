f255df89fcb968233cb48a47021c6ee56ca08caa
anomius <swarajpurohit@gmail.com>
2020-08-02 11:37:46 +0530

    cut

--- old_data/cut.py
+++ old_data/cut.py
@@ -5,11 +5,11 @@ start = timeit.default_timer()
 
 dt =np.dtype(np.int16)
 l= np.fromfile('black.rl0', dtype=dt).reshape((8238,6000))
-for i in range(20):
+for i in range(18):
     for j in range(25):
         x=320*j
-        y=300*i
-        k=l[x:x+320,y:y+300]
+        y=320*i
+        k=l[x:x+320,y:y+320]
         s=str(i)+" "+str(j)
         s="../"+s+'.tif'
         Image.fromarray(k).save(s)
